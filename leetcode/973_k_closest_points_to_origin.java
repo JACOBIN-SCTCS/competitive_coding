5791a6012970e92371d598cd6b60d98b7ec7a5c4
JACOBIN-SCTCS <jacobja3@gmail.com>
2021-07-25 15:55:54 +0530

    commit

---
+++ leetcode/973_k_closest_points_to_origin.java
@@ -0,0 +1,20 @@
+
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) 
+    {
+        Arrays.sort(points,new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a , int[] b)
+            {
+                return Integer.compare(a[0]*a[0]+a[1]*a[1],
+                                       b[0]*b[0]+b[1]*b[1]
+                                       
+                                      );
+            }
+            
+        });
+        
+        return Arrays.copyOfRange(points,0,k);
+    }
+}
