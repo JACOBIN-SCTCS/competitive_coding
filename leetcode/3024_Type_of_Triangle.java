75c154322ebdaf858f722e062d3bc2a61196dcdb
JACOB JAMES K <jacobja3@gmail.com>
2025-05-19 21:37:42 +0530

    Create 3024_Type_of_Triangle.java

---
+++ leetcode/3024_Type_of_Triangle.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String triangleType(int[] nums) {
+
+        Arrays.sort(nums);
+
+        if(nums[0] + nums[1] <= nums[2])
+            return "none";
+        else 
+        {
+            if(nums[0] == nums[1]  && nums[1] == nums[2])
+                return "equilateral";
+            else if(nums[0] == nums[1] || nums[1] == nums[2] || nums[2] == nums[0])
+                return "isosceles";
+            else
+                return "scalene";
+        }
+    }
+}
